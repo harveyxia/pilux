abd352f1cf1d1a31f910460facadeb6659146766
Harvey Xia <harvey@harveyxia.com>
2014-06-27 11:25:05 -0400

    Added untested transition function

--- lux.js
+++ lux.js
@@ -22,66 +22,75 @@ function blueOn(pwm) {
     // piblaster.releasePin(BLUE);
 }
 
-function light(r,g,b) {
-    redOn(r);
-    greenOn(g);
-    blueOn(b);
+var ledHandle = {
+    'r': redOn,
+    'g': greenOn,
+    'b': blueOn
 }
 
-function transition(i, j, k) {
-    console.log([i, j, k].join(','));
-    if (k !== 100) {
+function light(r,g,b) {
+    ledHandle.r(r);
+    ledHandle.g(g);
+    ledHandle.b(b);
+}
+
+// fades one color up or down, starting from state and ending at 0 | 100
+function fade(color, state, up, delay, next) {
+    console.log('Color=  ' + color + '\n' +
+                'State=  ' + state + '\n' +
+                'Up=   ' + up  + '\n' +
+                'Delay=  ' + delay + '\n'
+                );
+    if (up) {
         setTimeout(function() {
-            light(i,j,k);
-            transition(i,j,k+1);
-        }, 1);
+            ledHandle[color](state + 1);
+            if (state !== 100) {
+                fade(color, state + 1, 1, delay);
+            } else {
+                if (next) {
+                    console.log('==========NEXT==========');
+                    fade(next.color, next.state, next.up, next.delay, next.next);
+                }
+            }
+        }, delay);
     } else {
         setTimeout(function() {
-            light(i,j,k);
-            transition(i,j+1,0);
-        }, 1);
+            ledHandle[color](state - 1);
+            if (state !== 0) {
+                fade(color, state - 1, 0, delay);
+            } else {
+                if (next) {
+                    console.log('==========NEXT==========');
+                    fade(next.color, next.state, next.up, next.delay, next.next);
+                }
+            }
+        }, delay);
     }
-    // if (i != 100) {
+}
+
+// function transitionHelper(r, g, b, down, up) {
+//     var delay = 50;
+//     console.log([r, g, b].join(','));
+//     var red = fade('r', r, 0, delay);
+    // if (r != 100) {
     //     setTimeout(function() {
-    //         greenOn(i/100);
-    //         blueOn(j/100);
-    //         transition(i+1, 100-(i+1), 1);
+    //         light(r,g,b);
+    //         transitionHelper(r+1, Math.min(g - 1. 0), b);
     //     }, 50);
     // }
+// }
+// transition(1, 1, 1);
+ 
+function transition(delay) {
+    var phase1 = { color: 'g', state: 0, up: 1, delay: delay, next: phase2};
+    var phase2 = { color: 'r', state: 100, up: 0, delay: delay, next: phase3};
+    var phase3 = { color: 'b', state: 0, up: 1, delay: delay, next: phase4};
+    var phase4 = { color: 'g', state: 100, up: 0, delay: delay, next: phase5};
+    var phase5 = { color: 'r', state: 0, up: 1, delay: delay, next: phase6};
+    var phase6 = { color: 'g', state: 0, up: 1, delay: delay, next: null};
+    fade('r', 0, 1, delay, phase1);
 }
-transition(1, 1, 1);
-
-// rgb is hexademical representation
-function emit(rgb) {
-
-}
-
 
 module.exports = {
 
 }
-
-// module.exports = {
-//     greenOn: function() {
-        // gpio.open(12, 'output', function(err){
-        //     if (err) {
-        //         console.log(err);
-        //     } else {
-        //         gpio.write(12, 1, function() {
-        //             gpio.close(12);
-        //         });
-        //     }
-        // });
-//     },
-//     greenOff: function() {
-        // gpio.open(12, 'output', function(err){
-        //     if (err) {
-        //         console.log(err);
-        //     } else {
-        //         gpio.write(12, 0, function() {
-        //             gpio.close(12);
-        //         });
-        //     }
-        // });
-//     }
-// };
